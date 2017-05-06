6f74b15bac3a8a2fb684023150a4f652df2c208a
mrfangge <mickshifangge@gmail.com>
2017-05-07 00:39:34 +0800

    完成第二屏

--- src/js/index.js
+++ src/js/index.js
@@ -14,12 +14,19 @@ seajs.use(['jquery','app/main.js'],function () {
     // 全局$
     window.jQuery = window.$ = $;
     seajs.use('swiper',function () {
-        var swiper = new Swiper('.swiper-container', {
+        var swiper = new Swiper('#swiper1', {
             pagination: '.swiper-pagination',
             spaceBetween: 30,
             effect: 'fade',
             autoplay:1,
             speed:8000
+        });
+        var swiper = new Swiper('#swiper2', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            paginationBulletRender: function (swiper, index, className) {
+                return '<span class="'+className+' skydive-nav'+(index + 1) +'"></span>';
+            }
         });
     })
 
