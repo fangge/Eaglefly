a52ada40752997d81900f0a831f4118770f6272d
mrfangge <mickshifangge@gmail.com>
2017-05-08 01:06:16 +0800

    完成第三屏

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,57 @@ seajs.use(['jquery','app/main.js'],function () {
             },
             autoplay:10000
         });
+
+
+        var viewSwiper = new Swiper('.coach-view .swiper-container', {
+            effect: 'fade',
+            prevButton:'.prev-hidden-btn',
+            nextButton:'.next-hidden-btn',
+            onSlideChangeStart: function() {
+                updateNavPosition()
+            }
+        })
+
+        $('.coach-preview .prev-btn,.coach-view .prev-hidden-btn').on('click', function(e) {
+            e.preventDefault()
+            if (viewSwiper.activeIndex == 0) {
+                viewSwiper.slideTo(viewSwiper.slides.length - 1, 1000);
+                return
+            }
+            viewSwiper.slidePrev()
+        })
+        $('.coach-preview .next-btn,.coach-view .next-hidden-btn').on('click', function(e) {
+            e.preventDefault()
+            if (viewSwiper.activeIndex == viewSwiper.slides.length - 1) {
+                viewSwiper.slideTo(0, 1000);
+                return
+            }
+            viewSwiper.slideNext()
+        })
+
+        var previewSwiper = new Swiper('.coach-preview .swiper-container', {
+            slidesPerView: 6,
+            prevButton:'.prev-btn',
+            nextButton:'.next-btn',
+            spaceBetween:20,
+            onClick: function(swiper) {
+                viewSwiper.slideTo(swiper.clickedIndex)
+            }
+        })
+
+        function updateNavPosition() {
+            $('.coach-preview .preview-on').removeClass('preview-on');
+            var activeNav = $('.coach-preview .swiper-slide').eq(viewSwiper.activeIndex).addClass('preview-on');
+            if (!activeNav.hasClass('swiper-slide-visible')) {
+                if (activeNav.index() > previewSwiper.activeIndex) {
+                    var thumbsPerNav = Math.floor(previewSwiper.width / activeNav.width()) - 1
+                    previewSwiper.slideTo(activeNav.index() - thumbsPerNav)
+                } else {
+                    previewSwiper.slideTo(activeNav.index())
+                }
+            }
+        }
+
     })
 
 });
