65d6c5295e47813672e76a1e3e614f360a5fa022
Mrangge <mickshifangge@gmail.com>
2017-05-08 17:16:40 +0800

    完成首页

---
+++ dest/js/deps/header.js
@@ -0,0 +1,6 @@
+/*!
+ * @project : huya-assets
+ * @version : huya-client
+ * @author  : Mrfangge
+ * @update  : 2017-05-08 5:15:40 pm
+ */define("deps/header",[],function(require,exports,module){$("#nav").find(".nav-link").on("mouseenter",function(n){n.preventDefault();$("#nav").addClass("nav-on")}).on("mouseleave",function(n){n.preventDefault();$("#nav").removeClass("nav-on")})});
