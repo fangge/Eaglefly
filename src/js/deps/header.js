d7cb21a4685b824abdd876c5a34f1242acd74bcf
mrfangge <fangyang1@yy.com>
2017-05-11 22:09:18 +0800

    修改首页相关bug

--- src/js/deps/header.js
+++ src/js/deps/header.js
@@ -1,9 +1,18 @@
 define(function(require, exports, module) {
-   $('#nav').find('.nav-link').on('mouseenter',function (e) {
+   $('#nav:not(.nav-fixed)').find('.nav-link').on('mouseenter',function (e) {
        e.preventDefault();
        $('#nav').addClass('nav-on')
    }).on('mouseleave',function (e) {
        e.preventDefault();
        $('#nav').removeClass('nav-on')
    })
+
+    $(window).on('scroll', function() {
+        var now = $(window).scrollTop();
+        if(now>0){
+            $('#nav').addClass('nav-on nav-fixed')
+        }else{
+            $('#nav').removeClass('nav-on nav-fixed')
+        }
+    })
 })
